9736f842cbe1b688846a6815eca8808246e6a1fb
Rabire HAKIM <rabirehakim@MacBook-Air-de-Rabire.local>
2023-05-24 12:00:11 +0200

    feat(api): ✨ eslatic connection

--- tp2/index.js
+++ tp2/index.js
@@ -1,12 +1,29 @@
 import express from "express";
+import { Client } from "@elastic/elasticsearch";
+import fs from "fs";
 
 const PORT = 3000;
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
+const client = new Client({
+  node: "https://localhost:9200",
+  auth: {
+    username: "elastic",
+    password: "zkUFj8+jyb=W=B=PpXrq",
+  },
+  tls: {
+    ca: fs.readFileSync("./http_ca.crt"),
+    rejectUnauthorized: false,
+  },
+});
+
+app.get("/", async (req, res) => {
+  const movies = await client.search({
+    index: "movies",
+  });
+  res.send(movies);
 });
 
 app.listen(PORT, () => {
-  console.log(`Example app listening on PORT ${PORT}`);
+  console.log(`App listening on PORT ${PORT}`);
 });
